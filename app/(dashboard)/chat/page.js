9addf3188d2f69367364762345d547fd7eb68fde
= <=>
2024-09-10 14:48:12 +0500

    commit

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,14 +1,16 @@
-import Chat from '@/app/components/Chat'
-import React from 'react'
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
+import Chat from '@/components/Chat';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 
 const ChatPage = () => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Chat />
     </HydrationBoundary>
-  )
-}
-
-export default ChatPage
+  );
+};
+export default ChatPage;
